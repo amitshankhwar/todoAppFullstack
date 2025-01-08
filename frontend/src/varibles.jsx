57838d5835c907697a5ae5f0b773919f7c6075dc
amit shankhwar <88135890+amitshankhwar@users.noreply.github.com>
2025-01-08 12:49:25 +0530

    Update varibles.jsx

--- frontend/src/varibles.jsx
+++ frontend/src/varibles.jsx
@@ -1,2 +1,2 @@
-// export const baseURL = "https://todoappfullstack-kxv2.onrender.com";
-export const baseURL = "http://localhost:5000";
+export const baseURL = "https://todoappfullstack-kxv2.onrender.com";
+// export const baseURL = "http://localhost:5000";
